bfb4a3d29a8a4bbacc74f5878d21ce26380bc5c3
douglassouza1 <douglassouza12-@hotmail.com>
2019-10-02 10:53:38 -0300

    Novo

--- Projeto/MainActivity.cs
+++ Projeto/MainActivity.cs
@@ -1,7 +1,7 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Support.V7.App;
-using Android.Runtime;
+using Android.Content;
 using Android.Widget;
 
 namespace Projeto
@@ -9,11 +9,36 @@ namespace Projeto
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        EditText mensagem;
+        Button button1;
+        ImageView ImageView1;
+     
+
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            mensagem = FindViewById<EditText>(Resource.Id.editText1);
+            button1 = FindViewById<Button>(Resource.Id.button1);
+            ImageView1 = FindViewById<ImageView>(Resource.Id.imageView1);
+
+            button1.Click += delegate {
+
+                Intent i = new Intent(this, typeof(MainActivity2));
+                i.PutExtra("mensagem", mensagem.Text);
+                StartActivityForResult(i, 0);
+
+            };
+
+
+
+
+
+
+
         }
     }
 }
---
+++ Projeto/MainActivity2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Webkit;
+
+namespace Projeto
+{
+    [Activity(Label = "MainActivity2")]
+    public class MainActivity2 : Activity
+    {
+        
+        EditText segundo;
+        Button button2;
+        TextView textoVisor;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            SetContentView(Resource.Layout.layout2);
+
+
+            base.OnCreate(savedInstanceState);
+
+
+
+
+
+            segundo = FindViewById<EditText>(Resource.Id.segundo);
+            button2 = FindViewById<Button>(Resource.Id.button2);
+            textoVisor = FindViewById<TextView>(Resource.Id.textoVisor);
+
+
+
+            segundo.Text = Intent.GetStringExtra("mensagem");
+
+
+            button2.Click += delegate
+            {
+                Intent i = new Intent(this, typeof(MainActivity));
+                i.PutExtra("mensagem", segundo.Text);
+                StartActivityForResult(i, 0);
+
+
+
+            };
+
+
+
+
+            // Create your application here
+        }
+    }
+}
